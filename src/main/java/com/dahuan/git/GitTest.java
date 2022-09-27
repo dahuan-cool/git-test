4f97315c2354100be2774a4130796e476ba999b7
dahuan <dahuan@163.com>
2022-09-27 16:33:49 +0800

    four commit

--- src/main/java/com/dahuan/git/GitTest.java
+++ src/main/java/com/dahuan/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello,hello");
         System.out.println("hello,hello,hello");
-        System.out.println("hello,hello,hello,hello");
+        System.out.println("hello,hello,hello,cold-fix");
 
     }
 }
