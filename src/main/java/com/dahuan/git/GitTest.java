a5b9c5af0df436a77599dff22efd2c1927f07321
dahuan <dahuan@163.com>
2022-09-27 16:24:42 +0800

    second commit

--- src/main/java/com/dahuan/git/GitTest.java
+++ src/main/java/com/dahuan/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dahuan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello,hello");
     }
 }
